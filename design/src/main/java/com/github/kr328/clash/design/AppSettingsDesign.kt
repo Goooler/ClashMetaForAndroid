68586900b20dd1c00cee2c59a1037aeef712ee82
Goooler <wangzongler@gmail.com>
2026-04-20 17:45:15 +0800

    Rewrite NetworkSettingsDesign

--- design/src/main/java/com/github/kr328/clash/design/AppSettingsDesign.kt
+++ design/src/main/java/com/github/kr328/clash/design/AppSettingsDesign.kt
@@ -2,23 +2,16 @@ package com.github.kr328.clash.design
 
 import android.content.Context
 import android.view.View
-import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -28,11 +21,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.github.kr328.clash.design.component.SettingsCategoryTitle
 import com.github.kr328.clash.design.component.SettingsCommonScreen
+import com.github.kr328.clash.design.component.SettingsPreferenceClickableItem
+import com.github.kr328.clash.design.component.SettingsPreferenceSwitchItem
 import com.github.kr328.clash.design.model.Behavior
 import com.github.kr328.clash.design.model.DarkMode
 import com.github.kr328.clash.design.store.UiStore
@@ -43,7 +37,7 @@ import com.github.kr328.clash.service.store.ServiceStore
 class AppSettingsDesign(
   context: Context,
   uiStore: UiStore,
-  srvStore: ServiceStore,
+  serviceStore: ServiceStore,
   behavior: Behavior,
   running: Boolean,
   onHideIconChange: (hide: Boolean) -> Unit,
@@ -60,7 +54,7 @@ class AppSettingsDesign(
         darkModeInitial = uiStore.darkMode,
         hideAppIconInitial = uiStore.hideAppIcon,
         hideFromRecentsInitial = uiStore.hideFromRecents,
-        dynamicNotificationInitial = srvStore.dynamicNotification,
+        dynamicNotificationInitial = serviceStore.dynamicNotification,
         onAutoRestartChange = { behavior.autoRestart = it },
         onDarkModeChange = {
           uiStore.darkMode = it
@@ -74,7 +68,7 @@ class AppSettingsDesign(
           uiStore.hideFromRecents = it
           requests.trySend(Request.ReCreateAllActivities)
         },
-        onDynamicNotificationChange = { srvStore.dynamicNotification = it },
+        onDynamicNotificationChange = { serviceStore.dynamicNotification = it },
       )
     }
   }
@@ -104,7 +98,7 @@ private fun AppSettingsScreen(
 
   SettingsCommonScreen(title = stringResource(R.string.app), modifier = modifier.fillMaxSize()) {
     SettingsCategoryTitle(text = stringResource(R.string.behavior))
-    AppSettingsSwitchItem(
+    SettingsPreferenceSwitchItem(
       iconRes = R.drawable.ic_baseline_restore,
       titleRes = R.string.auto_restart,
       summaryRes = R.string.allow_clash_auto_restart,
@@ -116,13 +110,13 @@ private fun AppSettingsScreen(
     )
 
     SettingsCategoryTitle(text = stringResource(R.string.interface_))
-    AppSettingsClickableItem(
+    SettingsPreferenceClickableItem(
       iconRes = R.drawable.ic_baseline_brightness_4,
       titleRes = R.string.dark_mode,
-      summaryRes = darkMode.summaryRes(),
+      summaryRes = darkMode.summaryRes,
       onClick = { showDarkModeDialog = true },
     )
-    AppSettingsSwitchItem(
+    SettingsPreferenceSwitchItem(
       iconRes = R.drawable.ic_baseline_hide,
       titleRes = R.string.hide_app_icon_title,
       summaryRes = R.string.hide_app_icon_desc,
@@ -132,7 +126,7 @@ private fun AppSettingsScreen(
         onHideAppIconChange(it)
       },
     )
-    AppSettingsSwitchItem(
+    SettingsPreferenceSwitchItem(
       iconRes = R.drawable.ic_baseline_stack,
       titleRes = R.string.hide_from_recents_title,
       summaryRes = R.string.hide_from_recents_desc,
@@ -144,7 +138,7 @@ private fun AppSettingsScreen(
     )
 
     SettingsCategoryTitle(text = stringResource(R.string.service))
-    AppSettingsSwitchItem(
+    SettingsPreferenceSwitchItem(
       iconRes = R.drawable.ic_baseline_domain,
       titleRes = R.string.show_traffic,
       summaryRes = R.string.show_traffic_summary,
@@ -199,95 +193,14 @@ private fun AppSettingsScreen(
   }
 }
 
-@Composable
-private fun AppSettingsClickableItem(
-  @DrawableRes iconRes: Int,
-  @StringRes titleRes: Int,
-  @StringRes summaryRes: Int,
-  onClick: () -> Unit,
-  modifier: Modifier = Modifier,
-) {
-  Row(
-    modifier =
-      modifier
-        .fillMaxWidth()
-        .heightIn(min = 75.dp)
-        .clickable(onClick = onClick)
-        .padding(top = 16.dp, bottom = 16.dp, end = 20.dp),
-    verticalAlignment = Alignment.CenterVertically,
-  ) {
-    Spacer(modifier = Modifier.width(17.5.dp))
-    Icon(
-      painter = painterResource(iconRes),
-      contentDescription = null,
-      modifier = Modifier.size(30.dp),
-    )
-    Spacer(modifier = Modifier.width(17.5.dp))
-    Column {
-      Text(text = stringResource(titleRes), style = MaterialTheme.typography.bodyLarge)
-      Text(
-        text = stringResource(summaryRes),
-        style = MaterialTheme.typography.bodyMedium,
-        modifier = Modifier.padding(top = 5.dp),
-      )
+private val DarkMode.summaryRes: Int
+  @StringRes
+  get() =
+    when (this) {
+      DarkMode.Auto -> R.string.follow_system_android_10
+      DarkMode.ForceLight -> R.string.always_light
+      DarkMode.ForceDark -> R.string.always_dark
     }
-  }
-}
-
-@Composable
-private fun AppSettingsSwitchItem(
-  @DrawableRes iconRes: Int,
-  @StringRes titleRes: Int,
-  @StringRes summaryRes: Int,
-  checked: Boolean,
-  onCheckedChange: (Boolean) -> Unit,
-  modifier: Modifier = Modifier,
-  enabled: Boolean = true,
-) {
-  Row(
-    modifier = modifier.fillMaxWidth().heightIn(min = 75.dp).padding(top = 16.dp, bottom = 16.dp),
-    verticalAlignment = Alignment.CenterVertically,
-  ) {
-    Spacer(modifier = Modifier.width(17.5.dp))
-    Icon(
-      painter = painterResource(iconRes),
-      contentDescription = null,
-      modifier = Modifier.size(30.dp),
-      tint = if (enabled) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.outline,
-    )
-    Spacer(modifier = Modifier.width(17.5.dp))
-    Column(modifier = Modifier.weight(1f).padding(end = 12.dp)) {
-      Text(
-        text = stringResource(titleRes),
-        style = MaterialTheme.typography.bodyLarge,
-        color =
-          if (enabled) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.outline,
-      )
-      Text(
-        text = stringResource(summaryRes),
-        style = MaterialTheme.typography.bodyMedium,
-        color =
-          if (enabled) MaterialTheme.colorScheme.onSurfaceVariant
-          else MaterialTheme.colorScheme.outline,
-        modifier = Modifier.padding(top = 5.dp),
-      )
-    }
-    Switch(
-      checked = checked,
-      onCheckedChange = onCheckedChange,
-      enabled = enabled,
-      modifier = Modifier.padding(end = 20.dp),
-    )
-  }
-}
-
-private fun DarkMode.summaryRes(): Int {
-  return when (this) {
-    DarkMode.Auto -> R.string.follow_system_android_10
-    DarkMode.ForceLight -> R.string.always_light
-    DarkMode.ForceDark -> R.string.always_dark
-  }
-}
 
 @PreviewMihomo
 @Composable
