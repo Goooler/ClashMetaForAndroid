caaa6bd8e75325a456fc052582639a3e4705b918
Goooler <wangzongler@gmail.com>
2026-04-24 17:22:37 +0800

    Normal class

--- app/src/main/kotlin/com/github/kr328/clash/model/ProxyState.kt
+++ app/src/main/kotlin/com/github/kr328/clash/model/ProxyState.kt
@@ -3,7 +3,7 @@ package com.github.kr328.clash.model
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
-data class ProxyState(val nowState: MutableState<String>) {
+class ProxyState(private val nowState: MutableState<String>) {
   constructor(now: String) : this(mutableStateOf(now))
 
   val now: String
