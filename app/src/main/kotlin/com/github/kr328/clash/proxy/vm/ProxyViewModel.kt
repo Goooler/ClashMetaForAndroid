623c283c70e1db3f614d36d12ae625ea703d73e6
Goooler <wangzongler@gmail.com>
2026-04-27 10:28:05 +0800

    Revert "Revert nameIndexMap"
    This reverts commit 16664c5d8663daf3ffe6a97e2ef4bf5d13ece560.

--- app/src/main/kotlin/com/github/kr328/clash/proxy/vm/ProxyViewModel.kt
+++ app/src/main/kotlin/com/github/kr328/clash/proxy/vm/ProxyViewModel.kt
@@ -199,10 +199,11 @@ class ProxyViewModel(app: Application) : AndroidViewModel(app), DefaultLifecycle
 
       val sources =
         withContext(Dispatchers.Default) {
+          val nameIndexMap = names.withIndex().associate { (index, name) -> name to index }
           group.proxies.map { proxy ->
             UiState.ProxyItemSource(
               proxy = proxy,
-              linkIndex = if (proxy.type.group) names.indexOf(proxy.name) else -1,
+              linkIndex = if (proxy.type.group) nameIndexMap[proxy.name] ?: -1 else -1,
             )
           }
         }
