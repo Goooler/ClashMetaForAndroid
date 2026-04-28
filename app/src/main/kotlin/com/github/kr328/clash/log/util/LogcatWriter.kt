8486539d57bcca6cfc7c415bbd96aa3f6dfc5d42
Goooler <wangzongler@gmail.com>
2026-04-28 12:19:42 +0800

    Don't suspend appendMessage

--- app/src/main/kotlin/com/github/kr328/clash/log/util/LogcatWriter.kt
+++ app/src/main/kotlin/com/github/kr328/clash/log/util/LogcatWriter.kt
@@ -5,8 +5,6 @@ import com.github.kr328.clash.core.model.LogMessage
 import com.github.kr328.clash.model.LogFile
 import com.github.kr328.clash.util.logsDir
 import java.io.BufferedWriter
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class LogcatWriter(
   context: Context,
@@ -14,10 +12,8 @@ class LogcatWriter(
   private val writer: BufferedWriter = context.logsDir.resolve(file.fileName).bufferedWriter(),
 ) : AutoCloseable by writer {
 
-  suspend fun appendMessage(message: LogMessage) =
-    withContext(Dispatchers.IO) {
-      writer.appendLine(FORMAT.format(message.time.time, message.level.name, message.message))
-    }
+  fun appendMessage(message: LogMessage) =
+    writer.appendLine(FORMAT.format(message.time.time, message.level.name, message.message))
 }
 
 private const val FORMAT = "%d:%s:%s"
