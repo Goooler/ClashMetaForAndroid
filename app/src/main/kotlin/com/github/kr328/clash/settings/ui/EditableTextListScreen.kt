f80dfbb76981cf20c38151b2f5f31d0306591f8d
Goooler <wangzongler@gmail.com>
2026-04-29 11:14:14 +0800

    Still remember

--- app/src/main/kotlin/com/github/kr328/clash/settings/ui/EditableTextListScreen.kt
+++ app/src/main/kotlin/com/github/kr328/clash/settings/ui/EditableTextListScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
@@ -69,7 +68,7 @@ fun EditableTextListScreen(
   onDismiss: () -> Unit,
   onApply: (List<String>?) -> Unit,
 ) {
-  val values = rememberSaveable(initialValues) { initialValues.orEmpty().toMutableStateList() }
+  val values = remember(initialValues) { initialValues.orEmpty().toMutableStateList() }
   var showAddDialog by remember { mutableStateOf(false) }
 
   MihomoScaffold(
--- app/src/main/kotlin/com/github/kr328/clash/settings/ui/EditableTextMapScreen.kt
+++ app/src/main/kotlin/com/github/kr328/clash/settings/ui/EditableTextMapScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -73,7 +72,7 @@ fun EditableTextMapScreen(
   onApply: (Map<String, String>?) -> Unit,
 ) {
   val values =
-    rememberSaveable(initialValues) {
+    remember(initialValues) {
       mutableStateMapOf<String, String>().apply { putAll(initialValues.orEmpty()) }
     }
   val items by remember { derivedStateOf { values.entries.toList() } }
