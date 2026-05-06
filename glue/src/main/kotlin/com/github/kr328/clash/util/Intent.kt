42b248bc1d0b3b0ebb63a3f1ffcba734077481a9
Goooler <wangzongler@gmail.com>
2026-05-06 14:36:49 +0800

    Private mainActivityClass

--- glue/src/main/kotlin/com/github/kr328/clash/util/Intent.kt
+++ glue/src/main/kotlin/com/github/kr328/clash/util/Intent.kt
@@ -3,5 +3,7 @@ package com.github.kr328.clash.util
 import android.content.Context
 import android.content.Intent
 
+private val mainActivityClass = Class.forName("com.github.kr328.clash.MainActivity")
+
 fun mainIntent(context: Context, action: String? = null) =
-  Intent(context, Class.forName("com.github.kr328.clash.MainActivity")).setAction(action)
+  Intent(context, mainActivityClass).setAction(action)
