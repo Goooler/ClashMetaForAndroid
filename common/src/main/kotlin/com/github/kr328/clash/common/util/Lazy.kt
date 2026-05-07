86cf8978405830a0cf2c5de46336dc0443e686e1
Goooler <wangzongler@gmail.com>
2026-05-07 12:23:32 +0800

    Remove explicit LazyThreadSafetyMode

--- common/src/main/kotlin/com/github/kr328/clash/common/util/Lazy.kt
+++ common/src/main/kotlin/com/github/kr328/clash/common/util/Lazy.kt
@@ -1,5 +1,4 @@
 package com.github.kr328.clash.common.util
 
 @Suppress("NOTHING_TO_INLINE")
-inline fun <T : Any> unsafeLazy(noinline initializer: () -> T): Lazy<T> =
-  lazy(LazyThreadSafetyMode.NONE, initializer)
+inline fun <T : Any> unsafeLazy(noinline initializer: () -> T): Lazy<T> = lazy(NONE, initializer)
