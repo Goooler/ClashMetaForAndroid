6f57830a19c463d35191177aefd82a34d8a51bc7
Goooler <wangzongler@gmail.com>
2026-05-09 17:22:42 +0800

    Revert "Potential fix for pull request finding"
    This reverts commit f271fc718c8861b5b90f162926ea1aa80613d379.

--- glue/src/main/kotlin/com/github/kr328/clash/glue/remote/Remote.kt
+++ glue/src/main/kotlin/com/github/kr328/clash/glue/remote/Remote.kt
@@ -28,7 +28,7 @@ object Remote {
     }
 
   fun launch() {
-    Global.launch(Dispatchers.IO) { broadcasts.register() }
+    broadcasts.register()
     ApplicationObserver.attach(application)
     ApplicationObserver.onVisibleChanged {
       if (it) {
