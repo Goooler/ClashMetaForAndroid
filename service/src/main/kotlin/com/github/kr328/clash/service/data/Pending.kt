72b7119cb13e05100f2250839020b4f039f3723f
Goooler <wangzongler@gmail.com>
2026-05-09 16:12:11 +0800

    Remove ColumnInfo

--- service/src/main/kotlin/com/github/kr328/clash/service/data/Pending.kt
+++ service/src/main/kotlin/com/github/kr328/clash/service/data/Pending.kt
@@ -1,6 +1,5 @@
 package com.github.kr328.clash.service.data
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.TypeConverters
 import com.github.kr328.clash.service.model.Profile
@@ -9,14 +8,14 @@ import kotlin.uuid.Uuid
 @Entity(tableName = "pending", primaryKeys = ["uuid"])
 @TypeConverters(RoomTypeConverters::class)
 data class Pending(
-  @ColumnInfo(name = "uuid") val uuid: Uuid,
-  @ColumnInfo(name = "name") val name: String,
-  @ColumnInfo(name = "type") val type: Profile.Type,
-  @ColumnInfo(name = "source") val source: String,
-  @ColumnInfo(name = "interval") val interval: Long,
-  @ColumnInfo(name = "upload") val upload: Long,
-  @ColumnInfo(name = "download") val download: Long,
-  @ColumnInfo(name = "total") val total: Long,
-  @ColumnInfo(name = "expire") val expire: Long,
-  @ColumnInfo(name = "createdAt") val createdAt: Long = System.currentTimeMillis(),
+  val uuid: Uuid,
+  val name: String,
+  val type: Profile.Type,
+  val source: String,
+  val interval: Long,
+  val upload: Long,
+  val download: Long,
+  val total: Long,
+  val expire: Long,
+  val createdAt: Long = System.currentTimeMillis(),
 )
