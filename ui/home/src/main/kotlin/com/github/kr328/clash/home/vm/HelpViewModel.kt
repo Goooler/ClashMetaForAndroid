9df2737882d47fc7235e76d0f0b72da5431dba3d
Goooler <wangzongler@gmail.com>
2026-05-28 17:45:07 +0800

    New HelpApi

--- ui/home/src/main/kotlin/com/github/kr328/clash/home/vm/HelpViewModel.kt
+++ ui/home/src/main/kotlin/com/github/kr328/clash/home/vm/HelpViewModel.kt
@@ -7,20 +7,16 @@ import androidx.lifecycle.viewModelScope
 import com.github.kr328.clash.common.log.Log
 import com.github.kr328.clash.glue.util.TABBY_RELEASES_LATEST
 import com.github.kr328.clash.home.R
-import kotlinx.coroutines.Dispatchers
+import com.github.kr328.clash.home.api.HelpApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
 import net.swiftzer.semver.SemVer
-import okhttp3.OkHttpClient
-import okhttp3.Request
 
 internal class HelpViewModel(app: Application) : AndroidViewModel(app) {
+  private val api = HelpApi()
+
   val uiState: StateFlow<UiState>
     field = MutableStateFlow(UiState())
 
@@ -33,18 +29,7 @@ internal class HelpViewModel(app: Application) : AndroidViewModel(app) {
     viewModelScope.launch {
       uiState.update { it.copy(checkingForUpdates = true) }
       try {
-        val latestTag =
-          withContext(Dispatchers.IO) {
-            val request =
-              Request.Builder()
-                .url("https://api.github.com/repos/Goooler/Tabby/releases/latest")
-                .header("Accept", "application/json")
-                .build()
-            OkHttpClient().newCall(request).execute().use { response ->
-              if (!response.isSuccessful) return@withContext null
-              json.decodeFromString<GithubRelease>(response.body.string()).tagName
-            }
-          }
+        val latestTag = api.getLatestRelease()
 
         if (latestTag == null) {
           eventState.update {
@@ -86,8 +71,4 @@ internal class HelpViewModel(app: Application) : AndroidViewModel(app) {
 
     data class UpdateAvailable(val releasesUrl: String) : EventState
   }
-
-  @Serializable private data class GithubRelease(@SerialName("tag_name") val tagName: String)
 }
-
-val json = Json { ignoreUnknownKeys = true }
