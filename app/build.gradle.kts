9cf3df33f0796346cd4f09255897c37a7b626234
Goooler <wangzongler@gmail.com>
2026-05-30 13:18:53 +0800

    Prepare version 3.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
   defaultConfig {
     applicationId = "io.github.goooler.tabby"
     targetSdk = 35
-    versionName = "3.1.0"
+    versionName = "3.1.1"
     versionCode = checkNotNull(versionName).toVersionCode()
   }
 
