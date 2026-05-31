63f19563975bdc098009f546f0cce5c4ae987aaf
Goooler <wangzongler@gmail.com>
2026-05-31 23:21:40 +0800

    Update MIHOMO_CORE

--- glue/src/main/kotlin/com/github/kr328/clash/glue/util/Links.kt
+++ glue/src/main/kotlin/com/github/kr328/clash/glue/util/Links.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.net.toUri
 
-const val MIHOMO_CORE = "https://github.com/MetaCubeX/mihomo
+const val MIHOMO_CORE = "https://github.com/MetaCubeX/mihomo"
 const val MIHOMO_WIKI = "https://wiki.metacubex.one/"
 const val TABBY_REPO = "Goooler/Tabby"
 const val TABBY_GITHUB = "https://github.com/$TABBY_REPO"
