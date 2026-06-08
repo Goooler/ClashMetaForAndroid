6d3ed515be5d30c55ef39c52bf587e673e7d1495
Goooler <wangzongler@gmail.com>
2026-06-08 09:54:02 +0800

    Prefer get over androidApplication

--- ui/home/src/main/kotlin/com/github/kr328/clash/home/HomeModule.kt
+++ ui/home/src/main/kotlin/com/github/kr328/clash/home/HomeModule.kt
@@ -2,11 +2,10 @@ package com.github.kr328.clash.home
 
 import com.github.kr328.clash.home.vm.AndroidDependencies
 import com.github.kr328.clash.home.vm.HomeViewModel
-import org.koin.android.ext.koin.androidApplication
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val homeModule = module {
-  single<HomeViewModel.Dependencies> { AndroidDependencies(androidApplication()) }
+  single<HomeViewModel.Dependencies> { AndroidDependencies(application = get()) }
   viewModelOf(::HomeViewModel)
 }
