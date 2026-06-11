e305bd6718dc41f1753efb3634b96f77c118a894
Goooler <wangzongler@gmail.com>
2026-06-11 15:15:08 +0800

    Fix preview

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,12 +81,13 @@ allprojects {
         commonMain.dependencies {
           implementation(libs.jb.compose.resources)
         }
-
+        project.dependencies {
+          "androidRuntimeClasspath"(libs.androidx.compose.ui.tooling)
+        }
         androidMain.dependencies {
           implementation(libs.androidx.compose.ui)
           implementation(libs.androidx.compose.ui.tooling.preview)
           implementation(libs.androidx.compose.ui.util)
-          //          "debugImplementation"(libs.androidx.compose.ui.tooling)
           implementation(libs.androidx.compose.animation)
           implementation(libs.androidx.compose.material3)
           implementation(libs.androidx.lifecycle.viewmodel.compose)
