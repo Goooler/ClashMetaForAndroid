6dc987ace4ca858aec391c37d096ae1638b3147e
Goooler <wangzongler@gmail.com>
2026-06-17 21:49:43 +0800

    Migrate from Okhttp to Ktor

--- ui/home/src/androidMain/kotlin/com/github/kr328/clash/home/api/HelpApi.kt
+++ ui/home/src/androidMain/kotlin/com/github/kr328/clash/home/api/HelpApi.kt
@@ -1,30 +1,41 @@
 package com.github.kr328.clash.home.api
 
 import com.github.kr328.clash.glue.util.TABBY_REPO
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.accept
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.http.isSuccess
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
-import okhttp3.OkHttpClient
-import okhttp3.Request
 
 class HelpApi(
-  private val client: OkHttpClient = OkHttpClient(),
-  private val json: Json = Json { ignoreUnknownKeys = true },
-) {
-  suspend fun getLatestRelease(): String? =
-    withContext(Dispatchers.IO) {
-      val request =
-        Request.Builder()
-          .url("https://api.github.com/repos/$TABBY_REPO/releases/latest")
-          .header("Accept", "application/json")
-          .build()
-      client.newCall(request).execute().use { response ->
-        if (!response.isSuccessful) return@withContext null
-        json.decodeFromString<GithubRelease>(response.body.string()).tagName
+  private val client: HttpClient =
+    HttpClient(CIO) {
+      defaultRequest {
+        url("https://api.github.com/")
+        contentType(ContentType.Application.Json)
+        accept(ContentType.Application.Json)
+      }
+      install(ContentNegotiation) {
+        json(json = Json { ignoreUnknownKeys = true })
       }
     }
+) {
+  suspend fun getLatestRelease(): String? {
+    val response = client.get("repos/$TABBY_REPO/releases/latest")
+    return when {
+      response.status.isSuccess() -> response.body<GithubRelease>().tagName
+      else -> null
+    }
+  }
 }
 
 @Serializable data class GithubRelease(@SerialName("tag_name") val tagName: String)
