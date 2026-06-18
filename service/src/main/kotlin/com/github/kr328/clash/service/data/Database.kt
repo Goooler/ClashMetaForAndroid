54d64c9a4f5859b2309d8a5122bd0e86f915cee8
Goooler <wangzongler@gmail.com>
2026-06-18 10:24:40 +0800

    Remove extra TypeConverter for enum

--- service/src/main/kotlin/com/github/kr328/clash/service/data/Database.kt
+++ service/src/main/kotlin/com/github/kr328/clash/service/data/Database.kt
@@ -9,7 +9,6 @@ import androidx.room3.TypeConverters
 import androidx.room3.migration.Migration
 import androidx.sqlite.execSQL
 import com.github.kr328.clash.common.util.application
-import com.github.kr328.clash.service.model.Profile
 import kotlin.uuid.Uuid
 
 @DB(
@@ -51,15 +50,5 @@ object RoomTypeConverters {
   @TypeConverter
   fun toUUID(uuid: String): Uuid {
     return Uuid.parse(uuid)
-  }
-
-  @TypeConverter
-  fun fromProfileType(type: Profile.Type): String {
-    return type.name
-  }
-
-  @TypeConverter
-  fun toProfileType(type: String): Profile.Type {
-    return Profile.Type.valueOf(type)
   }
 }
