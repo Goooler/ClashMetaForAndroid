c3e95ade8ed967dabe34e88138be64f16a350499
Goooler <wangzongler@gmail.com>
2026-07-18 14:31:26 +0800

    Catalog convention plugin dependencies

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 dependencies {
   implementation(gradleKotlinDsl())
-  implementation("com.android.tools.build:gradle:${libs.versions.agp.get()}")
-  implementation("com.diffplug.spotless:spotless-plugin-gradle:${libs.plugins.spotless.get().version}")
-  implementation("org.jetbrains.compose:compose-gradle-plugin:${libs.versions.cmp.get()}")
-  implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:${libs.versions.kotlin.get()}")
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
+  implementation(libs.gradlePlugin.android)
+  implementation(libs.gradlePlugin.compose)
+  implementation(libs.gradlePlugin.kotlin)
+  implementation(libs.gradlePlugin.kotlin.composeCompiler)
+  implementation(libs.gradlePlugin.spotless)
 }
 
 gradlePlugin {
