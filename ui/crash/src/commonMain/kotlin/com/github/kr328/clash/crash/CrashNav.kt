e69216a5710c7be0470fcd29c5f0861bb5de8f37
Goooler <wangzongler@gmail.com>
2026-07-22 15:12:14 +0800

    Share CrashRoute and crashEntries

---
+++ ui/crash/src/commonMain/kotlin/com/github/kr328/clash/crash/CrashNav.kt
@@ -0,0 +1,13 @@
+package com.github.kr328.clash.crash
+
+import androidx.navigation3.runtime.EntryProviderScope
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+sealed interface CrashRoute : NavKey {
+  @Serializable data object ApkBroken : CrashRoute
+
+  @Serializable data object AppCrashed : CrashRoute
+}
+
+expect fun EntryProviderScope<NavKey>.crashEntries()
---
+++ ui/crash/src/jvmMain/kotlin/com/github/kr328/clash/crash/CrashNav.kt
@@ -0,0 +1,6 @@
+package com.github.kr328.clash.crash
+
+import androidx.navigation3.runtime.EntryProviderScope
+import androidx.navigation3.runtime.NavKey
+
+actual fun EntryProviderScope<NavKey>.crashEntries() = Unit
