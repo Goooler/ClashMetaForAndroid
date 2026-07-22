02c74b67753d65f4a153dcf608c24d23b8a9abfd
Goooler <wangzongler@gmail.com>
2026-07-22 16:24:44 +0800

    Revert "Separate home navigation by platform"
    This reverts commit befdbac07149baea1d3ca951e8043a875d3d1423.

--- ui/home/src/commonMain/kotlin/com/github/kr328/clash/home/HomeNav.kt
+++ ui/home/src/commonMain/kotlin/com/github/kr328/clash/home/HomeNav.kt
@@ -2,6 +2,8 @@ package com.github.kr328.clash.home
 
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
+import com.github.kr328.clash.home.ui.HelpScreen
+import com.github.kr328.clash.home.ui.HomeScreen
 import kotlinx.serialization.Serializable
 
 sealed interface HomeRoute : NavKey {
@@ -10,11 +12,23 @@ sealed interface HomeRoute : NavKey {
   @Serializable data object Help : HomeRoute
 }
 
-expect fun EntryProviderScope<NavKey>.homeEntries(
+fun EntryProviderScope<NavKey>.homeEntries(
   onOpenProxy: () -> Unit,
   onOpenProfiles: () -> Unit,
   onOpenProviders: () -> Unit,
   onOpenLogs: () -> Unit,
   onOpenSettings: () -> Unit,
   onOpenHelp: () -> Unit,
-)
+) {
+  entry<HomeRoute.Home> {
+    HomeScreen(
+      onOpenProxy = onOpenProxy,
+      onOpenProfiles = onOpenProfiles,
+      onOpenProviders = onOpenProviders,
+      onOpenLogs = onOpenLogs,
+      onOpenSettings = onOpenSettings,
+      onOpenHelp = onOpenHelp,
+    )
+  }
+  entry<HomeRoute.Help> { HelpScreen() }
+}
