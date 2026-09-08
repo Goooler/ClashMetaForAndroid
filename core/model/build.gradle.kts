bd6050f34927dfc66c33b11bb0aa8f5fa9a3344a
Goooler <wangzongler@gmail.com>
2026-09-08 16:46:43 +0800

    Remove extra namespace for core:model

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -4,10 +4,6 @@ plugins {
 }
 
 kotlin {
-  android {
-    namespace = "com.github.kr328.clash.core.model"
-  }
-
   sourceSets {
     commonMain.dependencies {
       api(libs.kotlin.serialization.json)
